1c6fb2ecffcdc04afb18f846ad28b92571ae13be
Hasib Shrabon <hasibahmedshrabon@gmail.com>
2025-12-09 15:46:15 +0600

    profile page and update profile complete

--- src/components/Modal/UpdateProfileModal.jsx
+++ src/components/Modal/UpdateProfileModal.jsx
@@ -1,50 +1,77 @@
 import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
-import React from "react";
+import { useForm } from "react-hook-form";
+import useAuth from "../../hooks/useAuth";
+import { imageUpload } from "../../../Utilites";
 
 const UpdateProfileModal = ({ closeModal, isOpen }) => {
+  const { register, handleSubmit, reset } = useForm();
+  const { user, updateUserProfile } = useAuth();
+
+  const onSubmit = async (data) => {
+    console.log("Updated Profile:", data);
+    const { name, profileImage } = data;
+    let uploadedImageUrl = user?.photoURL;
+
+    if (profileImage && profileImage.length > 0) {
+      uploadedImageUrl = await imageUpload(profileImage[0]);
+    }
+
+    await updateUserProfile(name, uploadedImageUrl);
+    closeModal();
+    reset();
+  };
+
   return (
     <div>
-      {" "}
       <Dialog
         open={isOpen}
         as="div"
-        className="relative z-10 focus:outline-none "
+        className="relative z-10 focus:outline-none"
         onClose={closeModal}
       >
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
           <div className="flex min-h-full items-center justify-center p-4">
             <DialogPanel
               transition
-              className="w-full max-w-md bg-white p-6 backdrop-blur-2xl duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0 shadow-xl rounded-2xl"
+              className="w-full max-w-md bg-white p-6 backdrop-blur-2xl 
+              duration-300 ease-out data-closed:transform-[scale(95%)] 
+              data-closed:opacity-0 shadow-xl rounded-2xl"
             >
               <DialogTitle
                 as="h3"
-                className="text-lg font-semibold text-center leading-6 py-8 text-blue-500"
+                className="text-lg font-semibold text-center leading-6 py-4 text-blue-500"
               >
                 Update your Profile
               </DialogTitle>
-              <form className="mt-4 space-y-4">
-                {/* Name */}
+
+              {/* Form */}
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="mt-4 space-y-4"
+              >
+                {/* Full Name */}
                 <div>
                   <label className="block text-sm font-medium text-blue-500">
                     Full Name
                   </label>
                   <input
                     type="text"
-                    value={name}
-                    required
-                    className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:ring-blue-500 focus:border-blue-500"
+                    {...register("name", { required: true })}
+                    defaultValue={user?.displayName}
+                    placeholder="Enter full name"
+                    className="mt-1 w-full rounded-md border border-gray-300 p-2 
+                    focus:ring-blue-500 focus:border-blue-500"
                   />
                 </div>
 
                 {/* Profile Image */}
                 <div>
                   <label className="block text-sm font-medium text-blue-500">
-                    Profile Image URL
+                    Profile Image
                   </label>
                   <input
                     type="file"
-                    placeholder="https://example.com/photo.jpg"
+                    {...register("profileImage")}
                     className="mt-1 w-full rounded-md border file-input file-input-primary"
                   />
                 </div>
